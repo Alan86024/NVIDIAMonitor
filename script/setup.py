a4b00d5e7e5905d079dc588e7aa446e31a878844
Jose Perez Huanca <perezhuancajose@gmail.com>
2024-09-27 23:01:41 -0300

    Se elimina cxFreeze

--- script/setup.py
+++
@@ -1,9 +0,0 @@
-from cx_Freeze import setup, Executable
-
-# Define la configuración del ejecutable
-setup(
-    name="nvidiaScript",
-    version="0.1",
-    description="Script NVIDIA",
-    executables=[Executable("script.py")],
-)
